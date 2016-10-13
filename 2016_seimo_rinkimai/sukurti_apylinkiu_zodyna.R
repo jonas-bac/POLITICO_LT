148067ca07b1dba88e91792f2502a8afb2f3eea4
Jonas B <jonas.bacelis@gmail.com>
2016-10-14 00:01:39 +0200

    modifikuotas apylinkiu zodynas ir jo scriptas (hdf5 index pridetas)

--- 2016_seimo_rinkimai/sukurti_apylinkiu_zodyna.R
+++ 2016_seimo_rinkimai/sukurti_apylinkiu_zodyna.R
@@ -1,12 +1,12 @@
 
 
-### sukurti apylinkiu zodyna (kartu su ju apygardom etc)
+### sukurti apylinkiu zodyna (kartu su ju apygardom), o taip pat HDF5 archyvavimo indeksa
 
 a = readLines("~/Biostuff/POLITIKA_LT/2016/raw_data/2016_vrk_raw_data_apyl_onlyParty.txt")
 
 ## get all voting station names
-apyg = grep("Puslapis atnaujintas",a) + 2     ### *!
-apyl = grep("^Puslapis atnaujintas",a)  + 4   ### *!
+apyg = grep("Puslapis atnaujintas",a) + 2
+apyl = grep("^Puslapis atnaujintas",a)  + 4
 map = data.frame(apyl=apyl,apyg=apyg,stringsAsFactors = F)
 
 transl3 = NULL # translator file for apygarda-apylinke
@@ -21,8 +21,8 @@ apyl_short = unlist(strsplit(a[map[i,"apyl"]],"[[:punct:]]|\\s"))[1]
 if (nchar(apyl_short)==1) apyl_short = unlist(strsplit(a[map[i,"apyl"]],"[[:punct:]]|\\s"))[3]
 if (nchar(apyl_short)==0) apyl_short = unlist(strsplit(a[map[i,"apyl"]],"[[:punct:]]|\\s"))[2]
 
-apyg_numbr = unlist(strsplit(a[map[i,"apyg"]]," \\(Nr\\.|\\) "))[2]
-apyl_numbr = unlist(strsplit(a[map[i,"apyl"]]," \\(Nr\\.|\\) "))[2]
+apyg_numbr = as.numeric(unlist(strsplit(a[map[i,"apyg"]]," \\(Nr\\.|\\) "))[2])
+apyl_numbr = as.numeric(unlist(strsplit(a[map[i,"apyl"]]," \\(Nr\\.|\\) "))[2])
 
 apygapyl_name = paste(apyg_short,apyl_short,sep="")
 apygapyl_numb = paste(apyg_numbr,apyl_numbr,sep="_")
@@ -59,6 +59,27 @@ transl3[which(transl3$abbrevi_name %in% dupl_nms[1:3]),]  # there are some, but 
 
 # doublecheck hash duplication
 sum(duplicated(transl3$apygapyl_hashed))
+
+# make sure that all are ordered based on apygardos NR and then apylinkes NR
+transl3 = transl3[order(transl3$apyg_nr,transl3$apyl_nr),]
+transl3$ordr = seq(nrow(transl3))
+
+
+##### for HDF5 storage and compression reasons, need to create an index coordinates
+tmp = as.data.frame(group_by(transl3,apyg_nr) %>% summarise(n_apyl=n()) %>% ungroup())
+max(tmp$n_apyl) # max_number of apylinkes (60)
+nrow(tmp) # max_number of apygardos (71)
+which(!tmp$apyg_nr %in% 1:71)  # any missing values (curiousity)
+
+z = 1:max(tmp$apyg_nr) # max number of apygardos (71)
+tmp$from = 1 + (z-1)*max(tmp$n_apyl)  # first instance index in storage matrix (max number of apylinkes)
+tmp$till = 0 + (z )*max(tmp$n_apyl)   # last instance index in storage matrix (max number of apylinkes)
+transl3 = merge(transl3,tmp,by="apyg_nr",all=T)
+transl3 = as.data.frame(group_by(transl3,apyg_nr) %>% mutate(apyl_rank=rank(apyl_nr)) %>% ungroup())
+
+## assign the final position in HDF5 file (assuming 60*71 columns)
+transl3$poz = transl3$from + transl3$apyl_rank - 1
+transl3[1:100,]
 
 write.table(transl3,"~/Dropbox/GIT/POLITIKA_LT/2016_seimo_rinkimai/apylinkiu_zodynas.txt",
             row.names = F,col.names = T,sep="\t",quote=F)
